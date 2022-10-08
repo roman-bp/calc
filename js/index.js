a5f1c2878665ff8eb3c39b106ed724ec7bf50973
Roman Danylyuk <98307615+roman-bp@users.noreply.github.com>
2022-10-08 12:05:49 +0300

    правки

--- js/index.js
+++ js/index.js
@@ -18,6 +18,10 @@ btnCalc1.onclick = () => {
     const iMc2 = document.querySelector('.inputMc2').value;
     const iPrice3 = document.querySelector('.inputPrice3').value;
     const iMc3 = document.querySelector('.inputMc3').value;
+    const iPrice4 = document.querySelector('.inputPrice4').value;
+    const iMc4 = document.querySelector('.inputMc4').value;
+    const iPrice5 = document.querySelector('.inputPrice5').value;
+    const iMc5 = document.querySelector('.inputMc5').value;
 
     document.querySelector('.screenMc1').innerHTML = iMc1;
     document.querySelector('.screenPrice1').innerHTML = iPrice1;
@@ -25,8 +29,11 @@ btnCalc1.onclick = () => {
     document.querySelector('.screenPrice2').innerHTML = iPrice2;
     document.querySelector('.screenMc3').innerHTML = iMc3;
     document.querySelector('.screenPrice3').innerHTML = iPrice3;
-
-    document.querySelector('.scrTotalPrice1').innerHTML = iPrice1 * iMc1 + iPrice2 * iMc2 + iPrice3 * iMc3;
+    document.querySelector('.screenMc4').innerHTML = iMc4;
+    document.querySelector('.screenPrice4').innerHTML = iPrice4;
+    document.querySelector('.screenMc5').innerHTML = iMc5;
+    document.querySelector('.screenPrice5').innerHTML = iPrice5;
+    document.querySelector('.scrTotalPrice1').innerHTML = iPrice1 * iMc1 + iPrice2 * iMc2 + iPrice3 * iMc3+ iPrice4 * iMc4+iPrice5 * iMc5;
     // return iPrice1 * iMc1;
 
 }
